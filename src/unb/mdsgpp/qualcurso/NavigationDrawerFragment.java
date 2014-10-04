4cfbf8b1820640d390d585267633b4d24dbd35c8
cmasmila <kmila2012@hotmail.com>
2014-10-04 18:52:26 -0300

    Fixing compound comment

--- src/unb/mdsgpp/qualcurso/NavigationDrawerFragment.java
+++ src/unb/mdsgpp/qualcurso/NavigationDrawerFragment.java
@@ -89,8 +89,10 @@ public class NavigationDrawerFragment extends Fragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
-		// Indicate that this fragment would like to influence the set of
-		// actions in the action bar.
+		/*
+		* Indicate that this fragment would like to influence the set of
+		* actions in the action bar.
+		*/
 		setHasOptionsMenu(true);
 	}
 
