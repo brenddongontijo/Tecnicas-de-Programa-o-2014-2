8025ae1fe8073126b045f0fcc5238f49c7be8332
Brenddon Gontijo <brenddongontijo@msn.com>
2014-11-28 12:12:48 -0200

    Logging system and documentation in AboutFragment, IndicatorListAdapter

--- src/unb/mdsgpp/qualcurso/CheckBoxListCallbacks.java
+++ src/unb/mdsgpp/qualcurso/CheckBoxListCallbacks.java
@@ -2,7 +2,14 @@ package unb.mdsgpp.qualcurso;
 
 import android.widget.CheckBox;
 
-	
+/**
+ * Class Name: CheckBoxListCallbacks
+ *
+ * This class is responsible to notify another Class of an asynchronous 
+ * action that has completed with success or error. Because Fragments 
+ * should be modular and a callback is used in the Fragment to call 
+ * methods in the Activity.
+ */
 public interface CheckBoxListCallbacks {
 	public void onCheckedItem(CheckBox checkbox);
 	public void onUnchekedItem(CheckBox checkbox) throws InstitutionNotFoundException;
--- src/unb/mdsgpp/qualcurso/IndicatorListAdapter.java
+++ src/unb/mdsgpp/qualcurso/IndicatorListAdapter.java
@@ -4,6 +4,7 @@ import helpers.Indicator;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.logging.Logger;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -14,8 +15,14 @@ import android.widget.TextView;
 
 /**
  * Class name: IndicatorListAdapter.
+ * 
+ * This class is responsible for create the adapter for a View that represents a
+ * list of all indicators.
  */
 public class IndicatorListAdapter extends ArrayAdapter<HashMap<String,String>> {
+	// Logging system.
+	private final static Logger LOGGER = Logger.getLogger(IndicatorListAdapter.
+			class.getName()); 
 	
 	// Use to keep the value of the indicator.
 	public static String INDICATOR_VALUE = "indicatorValue";
@@ -23,34 +30,56 @@ public class IndicatorListAdapter extends ArrayAdapter<HashMap<String,String>> {
 	// Use to keep a value.
 	public static String VALUE = "value";
 	
+	// Item layout selected.
 	private int itemLayout = 0;
 
+	// Constructor with context, text view id and indicator items.
 	public IndicatorListAdapter(Context context, int resource, List<HashMap<String,String>> items) {
 		super(context, resource, items);
 		this.itemLayout = resource;
 	}
 
+	/**
+	 * Get the previous view and displays a list of indicators.
+	 * 
+	 * @param position				Position of the item within the adapter's data.
+	 * @param convertView			The old view to reuse.
+	 * @param parent				The parent that this view will eventually be attached to.
+	 * @return
+	 */
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = convertView;
 
 		view = nullView(view);
 
+		// Getting indicators in their positions.
 		HashMap<String,String> hashMap = getItem(position);
 
 		if (hashMap != null) {
+			// Creating text view for indicator and his text.
 			TextView indicator = (TextView) view.findViewById(R.id.indicator);
 			TextView indicatorText = (TextView) view.findViewById(R.id.indicator_text);
-
-        	if (indicator != null) {
-            	indicator.setText(hashMap.get(VALUE));
-        	}
-        	
-        	if (indicatorText != null) {
-        		indicatorText.setText(Indicator.getIndicatorByValue(hashMap.get(INDICATOR_VALUE)).
+			
+			// Constants to verify is indicators textView are not null.
+			final boolean indicatorInitialized = (indicator != null);
+			final boolean indicatorTextInitialized = (indicatorText != null);
+			
+        	if (indicatorInitialized && indicatorTextInitialized) {
+            	// Setting value for indicator.
+        		indicator.setText(hashMap.get(VALUE));
+            	
+        		// Setting value for indicator text.
+            	indicatorText.setText(Indicator.getIndicatorByValue(hashMap.get(INDICATOR_VALUE)).
         				getSearchIndicatorName());
         	}
+        	else{
+        		LOGGER.warning("Out of memory to create indicators TextView!");
+        	}
     	}
+		else{
+			LOGGER.warning("Out of memory to create hashmap for indicators!");
+		}
 
     	return view;
 	}
@@ -63,14 +92,18 @@ public class IndicatorListAdapter extends ArrayAdapter<HashMap<String,String>> {
 	 * @return view
 	 */
 	private View nullView(View view) {
+		final boolean viewNotInitialized = (view == null);
 		
-		if (view == null) {
+		if (viewNotInitialized) {
+			// Inflating the view
 			LayoutInflater inflateView;
 			inflateView = LayoutInflater.from(getContext());
 			view = inflateView.inflate(itemLayout, null);
+			
+			LOGGER.info("IndicatorListAdapter View sucefully initialized!");
 		} 
 		else {
-			
+			LOGGER.warning("IndicatorListAdapter View not initialized!");
 		}
 		
 		return view;
